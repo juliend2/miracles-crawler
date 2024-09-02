2a8b3382333dc0ab6e01220424479f605d6f933e
Julien Desrosiers <juliend2@gmail.com>
2024-09-02 19:53:48 -0400

    Also get the categories, and put all that in a list

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from scrapy.crawler import CrawlerProcess
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
+    results = []
+
     def start_requests(self):
         urls = [
             'https://en.wikipedia.org/wiki/List_of_Marian_apparitions',
@@ -21,13 +23,26 @@ class QuotesSpider(scrapy.Spider):
                 if not meta_tr.css(year_selector).get():
                     continue
                 
-                yield {
-                    'name': meta_tr.css('th[data-sort-value]::text').get(),
+                result = {
+                    'category': meta_tr.css('td:first-child[title]').attrib['title'].strip(),
+                    'name': extract_text_with_spaces(meta_tr.css('th[data-sort-value]')),
                     'year': meta_tr.css(year_selector).get().strip(),
                     # 'tags': quote.css('div.tags a.tag::text').getall(),
                 }
-            
+                self.results.append(result)
+                yield result
+
+def extract_text_with_spaces(selector):
+    parts = selector.xpath('.//text() | .//br').getall()
+    text_with_spaces = ' '.join(
+        part.strip() if part != '<br>' else ' ' for part in parts
+    )
+    return text_with_spaces
+
 # Run the spider using CrawlerProcess
 process = CrawlerProcess()
 process.crawl(QuotesSpider)
 process.start()  # the script will block here until the crawling is finished
+
+spider = QuotesSpider()
+print(spider.results)
